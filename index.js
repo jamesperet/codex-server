138f3d1ec8376a794f6500db8443e49ff6858865
James Peret <james.peret@gmail.com>
2017-07-03 23:18:27 -0300

    Project organized

--- index.js
+++ index.js
@@ -1,35 +1,80 @@
+#!/usr/bin/env node
+
+'use strict';
+
 var express = require('express')
 var app = express()
+var path = require('path');
+var fs = require('fs');
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+//app.use('/public', express.static(path.join(__dirname + '/node_modules')));
+app.use(express.static('public'))
+
+
 
 var get_file = function(req, res){
-  fs = require('fs')
-  var path = ""
-  var file_type = ""
-  if(req.params['folder'] != undefined){
-    path = req.params['folder'] + "/"
+
+  var path = "";
+  var file_type = "";
+  var parts;
+  var extension;
+
+  if(req.params['folder_10'] != undefined){
+    path = path + req.params['folder_10'] + "/"
   }
+  if(req.params['folder_9'] != undefined){
+    path = path + req.params['folder_9'] + "/"
+  }
+  if(req.params['folder_8'] != undefined){
+    path = path + req.params['folder_8'] + "/"
+  }
+  if(req.params['folder_7'] != undefined){
+    path = path + req.params['folder_7'] + "/"
+  }
+  if(req.params['folder_6'] != undefined){
+    path = path + req.params['folder_6'] + "/"
+  }
+  if(req.params['folder_5'] != undefined){
+    path = path + req.params['folder_5'] + "/"
+  }
+  if(req.params['folder_4'] != undefined){
+    path = path + req.params['folder_4'] + "/"
+  }
+  if(req.params['folder_3'] != undefined){
+    path = path + req.params['folder_3'] + "/"
+  }
+  if(req.params['folder_2'] != undefined){
+    path = path + req.params['folder_2'] + "/"
+  }
+  if(req.params['folder_1'] != undefined){
+    path = path + req.params['folder_1'] + "/"
+  }
+  // Load file or look for index?
   if(req.params['file'] != undefined){
-    path = path + req.params['file']
-    parts = req.params['file'].split(".")
+    // Set filetype
+    path = path + req.params['file'];
+    parts = req.params['file'].split(".");
     extension = parts[parts.length - 1]
     if(extension == "md"){
       file_type = "markdown"
     }
-  }
-  fs.readFile(path, 'utf8', function (err,data) {
-    if (err) {
-      return console.log(err);
-    }
-    process_file(res, data, file_type)
-  });
-}
-
-var process_file = function(res, data, file_type){
-  if(file_type == "markdown"){
-    res.send(markdown_parser(data))
   } else {
-    res.send(data)
+    // return index file if it exists
   }
+  // Action for each filetype
+  if(file_type == "markdown"){
+    fs.readFile(path, 'utf8', function (err,data) {
+      if (err) {
+        return console.log(err);
+      }
+      res.render('index', { body: markdown_parser(data) });
+    });
+  } else {
+    res.sendFile( process.cwd() + "/" + path)
+  }
+
 }
 
 var markdown_parser = function(data){
@@ -50,17 +95,56 @@ var markdown_parser = function(data){
   // var md = require('markdown-it')()
   //             .use(require('markdown-it-mathjax')());
 
-  var md = require('markdown-it')()
-        .use(require('markdown-it-math'), {
-            inlineOpen: '\(',
-            inlineClose: '\\)',
-            blockOpen: '\\[',
-            blockClose: '\\]'
-        })
+  var md = require('markdown-it')({
+      html: true,
+      linkify: true,
+      typographer: true
+    }).use(require('markdown-it-math'), {
+      inlineOpen: '\\(',
+      inlineClose: '\\)',
+      blockOpen: '\\[',
+      blockClose: '\\]'
+    }).use(require('markdown-it-highlightjs'), {auto: true, code: false})
   return md.render(data)
 }
 
-app.get('/:folder/:file', function (req, res) {
+app.get('/:folder_10/:folder_9/:folder_8/:folder_7/:folder_6/:folder_5/:folder_4/:folder_3/:folder_2/:folder_1/:file', function (req, res) {
+  get_file(req, res);
+})
+
+app.get('/:folder_9/:folder_8/:folder_7/:folder_6/:folder_5/:folder_4/:folder_3/:folder_2/:folder_1/:file', function (req, res) {
+  get_file(req, res);
+})
+
+app.get('/:folder_8/:folder_7/:folder_6/:folder_5/:folder_4/:folder_3/:folder_2/:folder_1/:file', function (req, res) {
+  get_file(req, res);
+})
+
+app.get('/:folder_7/:folder_6/:folder_5/:folder_4/:folder_3/:folder_2/:folder_1/:file', function (req, res) {
+  get_file(req, res);
+})
+
+app.get('/:folder_6/:folder_5/:folder_4/:folder_3/:folder_2/:folder_1/:file', function (req, res) {
+  get_file(req, res);
+})
+
+app.get('/:folder_5/:folder_4/:folder_3/:folder_2/:folder_1/:file', function (req, res) {
+  get_file(req, res);
+})
+
+app.get('/:folder_4/:folder_3/:folder_2/:folder_1/:file', function (req, res) {
+  get_file(req, res);
+})
+
+app.get('/:folder_3/:folder_2/:folder_1/:file', function (req, res) {
+  get_file(req, res);
+})
+
+app.get('/:folder_2/:folder_1/:file', function (req, res) {
+  get_file(req, res);
+})
+
+app.get('/:folder_1/:file', function (req, res) {
   get_file(req, res);
 })
 
