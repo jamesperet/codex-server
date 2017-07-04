6cc603d3aa1be24283c49934761e67c293280277
James Peret <james.peret@gmail.com>
2017-07-03 23:40:32 -0300

    New npm package name

--- index.js
+++ index.js
@@ -153,5 +153,5 @@ app.get('/:file', function (req, res) {
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Codex Server – listening on port 3000')
 })
