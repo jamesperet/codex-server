e072baebc2938065a87a32ee95b220ff3acf9a10
James Peret <james.peret@gmail.com>
2018-08-02 21:51:15 -0300

    Fixed the markdown-it-mermeid problem

--- index.js
+++ index.js
@@ -222,12 +222,14 @@ var markdown_parser = function(data){
       html: true,
       linkify: true,
       typographer: true
-    }).use(require('markdown-it-math'), {
+    })
+    .use(require('markdown-it-mermaid').default)
+    .use(require('markdown-it-math'), {
       inlineOpen: '\\(',
       inlineClose: '\\)',
       blockOpen: '\\[',
       blockClose: '\\]'
-    }).use(require('markdown-it-mermaid'))
+    })
     //.use(require('markdown-it-highlightjs'), {auto: true, code: false})
   return md.render(data)
 }
